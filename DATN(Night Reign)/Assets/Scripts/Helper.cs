c7d83dd4a507b0cac4d04b74aeb1b2503aec9700
duyyyyyyyyy <duytnps41276@gmail.com>
2025-03-14 01:52:18 +0700

    duy update
    DoubleL (1 tay), LongsSword(2 tay)

---
+++ DATN(Night Reign)/Assets/Scripts/Helper.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Helper : MonoBehaviour
+{
+    [Range(0, 1)]
+    Animator anim;
+    public float vertical;
+    public string[] oneHand;
+    public string[] twoHands;
+    public bool playAnim;
+    public bool twoHanded;
+    public bool enableRM;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        enableRM = !anim.GetBool("canMove");
+        anim.applyRootMotion = enableRM;
+        if(enableRM)
+            return;
+        anim.SetBool("two_handed", twoHanded);
+        if(playAnim)
+        {
+            string targetAnim;
+            if(!twoHanded)
+            {
+                int r = Random.Range(0, oneHand.Length);
+                targetAnim = oneHand[r];
+            }
+            else
+            {
+                int r = Random.Range(0, twoHands.Length);
+                targetAnim = twoHands[r];
+
+            }
+            vertical = 0;
+/*            anim.SetBool("canMove", false);
+            enabled = true;*/
+            anim.CrossFade(targetAnim, 0.2f);
+            playAnim = false;
+        }
+        anim.SetFloat("vertical", vertical);
+    }
+    public void SendEvent()
+    {
+        Debug.Log("Animation Event SendEvent được gọi!");
+    }
+}
---
+++ DATN(Night Reign)/Assets/Scripts/KeepBool.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class KeepBool : StateMachineBehaviour
+{
+    public string boolName;
+    public bool status;
+    public bool resetOnExit = true;
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool(boolName, status);
+    }
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (resetOnExit)
+            animator.SetBool(boolName, !status);
+
+    }
+}
