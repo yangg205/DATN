b94ac894ae46185d5ca47158734d1418e49def7e
duyyyyyyyyy <duytnps41276@gmail.com>
2025-03-23 18:06:52 +0700

    duy

---
+++ DATN(Night Reign)/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,46 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int healthLevel = 10;
+    public int maxHealth;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    AnimatorHandler animatorHandler;
+
+    private void Awake()
+    {
+        animatorHandler = GetComponentInChildren<AnimatorHandler>();
+    }
+    void Start()
+    {
+        maxHealth = SetMaxHealthFromHealthLevel();
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    private int SetMaxHealthFromHealthLevel()
+    {
+        maxHealth = healthLevel * 10;
+        return maxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth = currentHealth - damage;
+
+        healthBar.SetCurrenHealth(currentHealth);
+
+        animatorHandler.PlayTargetAnimation("Hit", true);
+
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            animatorHandler.PlayTargetAnimation("Death", true);
+
+            //handle player dead
+        }    
+    }
+}
