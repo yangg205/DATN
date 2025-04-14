dbc97faf7ef4fd0ffeed6646e7e98f885cf288e7
LeVanCaoLam <levancaolam05@gmail.com>
2025-04-14 14:51:12 +0700

    Update setting audio
    Update setting audio

--- DATN(Night Reign)/Assets/Scripts/MainMenu.cs
+++ DATN(Night Reign)/Assets/Scripts/MainMenu.cs
@@ -1,5 +1,6 @@
-using Fusion;
+﻿using Fusion;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : NetworkBehaviour
@@ -7,6 +8,10 @@ public class MainMenu : NetworkBehaviour
     public void LoadGame()
     {
         SceneManager.LoadScene("Lam");
+    }
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene("Settings"); // thay "Settings" bằng tên scene thực tế nếu khác
     }
     public void ExitGame()
     {
---
+++ DATN(Night Reign)/Assets/Scripts/settingAudio.cs
@@ -0,0 +1,264 @@
+﻿//using UnityEngine;
+//using UnityEngine.UI;
+//using Fusion;
+
+//public class settingAudio : MonoBehaviour         //              (1)or(2) -->  (3)
+//{                                                //Start +... --> SetFloat -->  GetFloat
+//                                                 //               Save           load (*.value is get)
+//    [SerializeField] Slider Slider;
+//    [SerializeField] Text textUI;
+//    float num;
+
+//    void Start()
+//    {   
+//        if (PlayerPrefs.HasKey("sldkey"))// is value save before (2)
+//            load();//3
+//        else
+//        {
+//            PlayerPrefs.SetFloat("sldkey", 1);// setFloat is value first time (1)" we can chose value 
+//            load();//(3)
+//        }
+//    }
+//    public void Change()
+//    {
+//        AudioListener.volume = Slider.value;
+//        num = Slider.value * 100;
+//        textUI.text = num.ToString();
+//        save(); // change and save in the same time ;
+//    }
+//    public void save()
+//    {
+//        PlayerPrefs.SetFloat("sldkey",Slider.value);
+//        load();
+//    }
+//    public void load()
+//    {
+//        Slider.value = PlayerPrefs.GetFloat("sldkey");
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+//using UnityEngine;
+//using UnityEngine.UI;
+//using Fusion;
+
+//public class settingAudio : MonoBehaviour
+//{
+//    [SerializeField] Slider Slider;
+//    [SerializeField] Text textUI;
+//    [SerializeField] Image buttonImage;
+//    [SerializeField] Sprite spriteOn;     // hình khi bật âm
+//    [SerializeField] Sprite spriteOff;    // hình khi tắt âm
+
+//    float num;
+//    float previousVolume = 1f; // lưu volume trước khi tắt
+//    bool isMuted = false;
+
+//    void Start()
+//    {
+//        if (PlayerPrefs.HasKey("sldkey"))
+//            load();
+//        else
+//        {
+//            PlayerPrefs.SetFloat("sldkey", 1);
+//            load();
+//        }
+
+//        UpdateAudioUI();
+//        UpdateButtonImage();
+//    }
+
+//    public void Change()
+//    {
+//        AudioListener.volume = Slider.value;
+//        num = Slider.value * 100;
+//        textUI.text = num.ToString("F0");
+
+//        isMuted = (Slider.value <= 0);
+//        if (!isMuted)
+//            previousVolume = Slider.value;
+
+//        save();
+//        UpdateButtonImage();
+//    }
+
+//    public void save()
+//    {
+//        PlayerPrefs.SetFloat("sldkey", Slider.value);
+//        load();
+//    }
+
+//    public void load()
+//    {
+//        Slider.value = PlayerPrefs.GetFloat("sldkey");
+//        AudioListener.volume = Slider.value;
+//        isMuted = (Slider.value <= 0);
+//        if (!isMuted)
+//            previousVolume = Slider.value;
+
+//        UpdateAudioUI();
+//        UpdateButtonImage();
+//    }
+
+//    // Toggle bật / tắt nhạc
+//    public void ToggleMute()
+//    {
+//        if (!isMuted)
+//        {
+//            // Lưu lại volume trước khi tắt
+//            previousVolume = Slider.value;
+//            AudioListener.volume = 0;
+//            Slider.value = 0;
+//            isMuted = true;
+//        }
+//        else
+//        {
+//            // Bật lại âm thanh
+//            Slider.value = previousVolume;
+//            AudioListener.volume = previousVolume;
+//            isMuted = false;
+//        }
+
+//        textUI.text = (Slider.value * 100).ToString("F0");
+//        save();
+//        UpdateButtonImage();
+//    }
+
+//    private void UpdateAudioUI()
+//    {
+//        num = Slider.value * 100;
+//        textUI.text = num.ToString("F0");
+//    }
+
+//    private void UpdateButtonImage()
+//    {
+//        if (buttonImage != null)
+//        {
+//            buttonImage.sprite = isMuted ? spriteOff : spriteOn;
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; // <- THÊM DÒNG NÀY
+using Fusion;
+
+public class settingAudio : MonoBehaviour
+{
+    [SerializeField] Slider Slider;
+    [SerializeField] Text textUI;
+    [SerializeField] Image buttonImage;
+    [SerializeField] Sprite spriteOn;
+    [SerializeField] Sprite spriteOff;
+
+    float num;
+    float previousVolume = 1f;
+    bool isMuted = false;
+
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("sldkey"))
+            load();
+        else
+        {
+            PlayerPrefs.SetFloat("sldkey", 1);
+            load();
+        }
+
+        UpdateAudioUI();
+        UpdateButtonImage();
+    }
+
+    public void Change()
+    {
+        AudioListener.volume = Slider.value;
+        num = Slider.value * 100;
+        textUI.text = num.ToString("F0");
+
+        isMuted = (Slider.value <= 0);
+        if (!isMuted)
+            previousVolume = Slider.value;
+
+        save();
+        UpdateButtonImage();
+    }
+
+    public void save()
+    {
+        PlayerPrefs.SetFloat("sldkey", Slider.value);
+        load();
+    }
+
+    public void load()
+    {
+        Slider.value = PlayerPrefs.GetFloat("sldkey");
+        AudioListener.volume = Slider.value;
+        isMuted = (Slider.value <= 0);
+        if (!isMuted)
+            previousVolume = Slider.value;
+
+        UpdateAudioUI();
+        UpdateButtonImage();
+    }
+
+    public void ToggleMute()
+    {
+        if (!isMuted)
+        {
+            previousVolume = Slider.value;
+            AudioListener.volume = 0;
+            Slider.value = 0;
+            isMuted = true;
+        }
+        else
+        {
+            Slider.value = previousVolume;
+            AudioListener.volume = previousVolume;
+            isMuted = false;
+        }
+
+        textUI.text = (Slider.value * 100).ToString("F0");
+        save();
+        UpdateButtonImage();
+    }
+
+    private void UpdateAudioUI()
+    {
+        num = Slider.value * 100;
+        textUI.text = num.ToString("F0");
+    }
+
+    private void UpdateButtonImage()
+    {
+        if (buttonImage != null)
+        {
+            buttonImage.sprite = isMuted ? spriteOff : spriteOn;
+        }
+    }
+
+    // ✅ Hàm mới: Quay lại scene Menu
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu"); // Thay "Menu" bằng tên scene menu thực tế nếu khác
+    }
+}
+
