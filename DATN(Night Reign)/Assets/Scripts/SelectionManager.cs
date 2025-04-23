15ae452cf98c4460e823e162cdbf9c9eefe1cb3c
duyyyyyyyyy <duytnps41276@gmail.com>
2025-04-23 18:31:08 +0700

    duy

--- DATN(Night Reign)/Assets/Scripts/SelectionManager.cs
+++ DATN(Night Reign)/Assets/Scripts/SelectionManager.cs
@@ -47,7 +47,7 @@ public class SelectionManager : MonoBehaviour
         StartCoroutine(SelectCharacter(id, characterid));
 
         // Chuyển sang scene "Duy"
-        SceneManager.LoadScene("Duy");
+        SceneManager.LoadScene("Loading");
         playerid = id;
         Debug.Log("Player ID: " + playerid);
 
