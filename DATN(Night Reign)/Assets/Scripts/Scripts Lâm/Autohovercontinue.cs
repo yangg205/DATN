c2b2ed772ae78774e9876a3d53a982cf22d17976
LeVanCaoLam <levancaolam05@gmail.com>
2025-06-08 18:49:37 +0700

    update continue autohover Status

---
+++ DATN(Night Reign)/Assets/Scripts/Scripts Lâm/Autohovercontinue.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class AutoOutline : MonoBehaviour
+{
+    private Outline outline;
+    private bool isSelected = false;
+    [SerializeField] bool status= false;
+
+    private void Start()
+    {
+        outline = GetComponent<Outline>();    }
+
+    private void Update()
+    {
+        if (status == true)
+        {
+            if (outline != null)
+            {
+                outline.enabled = true; // Hiển thị sẵn Outline
+                outline.effectColor = Color.yellow; // Có thể đổi màu nếu muốn khác lúc hover
+            }
+        }
+
+    }
+
+    public void Deselect()
+    {
+        isSelected = false;
+        if (outline != null)
+        {
+            outline.effectColor = Color.yellow; // Quay về màu hover ban đầu
+        }
+    }
+}
