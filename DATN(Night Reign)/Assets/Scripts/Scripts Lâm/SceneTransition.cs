a015c55afecec5284ef60057f40d5eb938bc70b8
yangg205 <167543231+yangg205@users.noreply.github.com>
2025-06-18 23:22:45 +0700

    auth

--- DATN(Night Reign)/Assets/Scripts/Scripts Lâm/SceneTransition.cs
+++ DATN(Night Reign)/Assets/Scripts/Scripts Lâm/SceneTransition.cs
@@ -2,24 +2,43 @@
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
+using NUnit.Framework;
 
 public class SceneTransition : MonoBehaviour
 {
     public Image fadeImage;
+    SignalRClient signalRClient;
+    int playerId;
+    int[] characterId = { 5, 1005, 1006 };
+    NotificationManager notificationManager;
 
     void Start()
     {
+        playerId = PlayerPrefs.GetInt("PlayerId", 0);
+        signalRClient = FindAnyObjectByType<SignalRClient>();
+        notificationManager = FindAnyObjectByType<NotificationManager>();
         // Fade in: từ đen -> trong suốt
         fadeImage.color = new Color(0, 0, 0, 1);
         fadeImage.DOFade(0, 1f).SetEase(Ease.InOutQuad);
     }
 
-    public void FadeToScene(string sceneName)
+    public async void FadeToScene(string sceneName)
     {
-        // Fade out: từ trong suốt -> đen
-        fadeImage.DOFade(1, 1f).SetEase(Ease.InOutQuad).OnComplete(() =>
+        int count = 0;
+        for(int i = 0; i < 3; i++)
+        {
+            var result = await signalRClient.SelectedCharacter(playerId, characterId[i]);
+            if (result.status)
+            {
+                count++;
+            }
+        }
+        if(count > 0)
+        {
+            fadeImage.DOFade(1, 1f).SetEase(Ease.InOutQuad).OnComplete(() =>
         {
             SceneManager.LoadScene(sceneName);
         });
+        }
     }
 }
