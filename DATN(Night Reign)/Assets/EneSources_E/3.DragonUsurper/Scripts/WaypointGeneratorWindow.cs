c73e4ef964a02a9cc85be9179120cd02488934eb
Duyen Nguyen <davichannel023@gmail.com>
2025-06-18 19:33:59 +0700

    fix thu vien

--- DATN(Night Reign)/Assets/EneSources_E/3.DragonUsurper/Scripts/WaypointGeneratorWindow.cs
+++ DATN(Night Reign)/Assets/EneSources_E/3.DragonUsurper/Scripts/WaypointGeneratorWindow.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -143,3 +144,4 @@ public class WaypointGeneratorWindow : EditorWindow
         }
     }
 }
+#endif
