efe1dd094b15cd8a009394b252c6ec1e315e8a86
LeVanCaoLam <levancaolam05@gmail.com>
2025-07-08 16:21:32 +0700

    update ParticleSystem

---
+++ DATN(Night Reign)/Assets/Scripts/Scripts Lâm/ParticleLoopController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleResetLoop : MonoBehaviour
+{
+    public float resetInterval = 3f; // thời gian giữa các lần reset
+
+    private ParticleSystem ps;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        StartCoroutine(LoopParticle());
+    }
+
+    IEnumerator LoopParticle()
+    {
+        while (true)
+        {
+            ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            ps.Play();
+            yield return new WaitForSeconds(resetInterval);
+        }
+    }
+}
