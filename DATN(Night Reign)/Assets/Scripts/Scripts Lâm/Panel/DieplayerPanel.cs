4a7c881937f01183a33330eafc3eb5c030fdba60
LeVanCaoLam <levancaolam05@gmail.com>
2025-07-29 17:33:52 +0700

    panel killcoinboss và dieplayer

---
+++ DATN(Night Reign)/Assets/Scripts/Scripts Lâm/Panel/DieplayerPanel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class DieplayerPanel : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public float fadeDuration = 0.5f;
+
+    private void Awake()
+    {
+        if (canvasGroup == null) canvasGroup = GetComponent<CanvasGroup>();
+        if (canvasGroup == null) canvasGroup = gameObject.AddComponent<CanvasGroup>();
+
+        // Chỉ làm trong suốt, không tắt hẳn GameObject
+        canvasGroup.alpha = 0f;
+    }
+
+    public void ShowPanel()
+    {
+        gameObject.SetActive(true);  // Đảm bảo panel bật
+        canvasGroup.DOFade(1f, fadeDuration).SetEase(Ease.OutCubic);
+    }
+
+    public void HidePanel()
+    {
+        canvasGroup.DOFade(0f, fadeDuration);
+    }
+}
