22a609fedb1549fe3348e277bbb11f638608630b
tiendat1708 <dattrinh1708@gmail.com>
2025-08-26 14:33:23 +0700

    music boss arena

---
+++ DATN(Night Reign)/Assets/Package/Dat/Scripts/BossTriggerZone.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BossTriggerZone : MonoBehaviour
+{
+    public AudioClip bossMusic; // Kéo file nhạc vào đây trong Inspector
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // chỉ Player mới kích hoạt
+        {
+            MusicManager.Instance.PlayMusic(bossMusic, true);
+            Debug.Log("Boss music started!");
+        }
+    }
+}
---
+++ DATN(Night Reign)/Assets/Package/Dat/Scripts/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        // Singleton pattern
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Giữ lại khi đổi scene
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.loop = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMusic(AudioClip clip, bool loop = true)
+    {
+        if (clip == null) return;
+        if (audioSource.clip == clip) return; // tránh phát lại cùng nhạc
+
+        audioSource.clip = clip;
+        audioSource.loop = loop;
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+}
